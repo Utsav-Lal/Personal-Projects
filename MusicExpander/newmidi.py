f3a6c16f855ffdcd67b9ea05ba16c53133633cc0
Utsav Lal <mailutsavlal@gmail.com>
2025-03-15 00:25:11 -0700

    Cleanup newmidi.py 3/15

--- MusicExpander/newmidi.py
+++ MusicExpander/newmidi.py
@@ -6,51 +6,42 @@ import math
 from iden import iden
 import sys
 
-notes = []
-notes2 = []
-# file = 'nuclear_fusion.midi'
+
+# Handles the input file
 file = None
 if file is None:
     if len(sys.argv) != 1:
         file = sys.argv[1]
     else:
         file = input("Enter file name: ")
+
+
+# Collects all the notes in the midi file
+notes = []
 midi_data = pretty_midi.PrettyMIDI(file)
-midi_data2 = pretty_midi.PrettyMIDI(file)
-print("duration:",midi_data.get_end_time())
-print(f'{"note":>10} {"start":>10} {"end":>10}')
 for instrument in midi_data.instruments:
-    print("instrument:", instrument.program);
     for note in instrument.notes:
-        print(f'{note.pitch:10} {note.start:10} {note.end:10}')
         notes.append((note.pitch, note.start, note.end))
 
-for instrument in midi_data2.instruments:
-    print("instrument:", instrument.program);
-    for note in instrument.notes:
-        print(f'{note.pitch:10} {note.start:10} {note.end:10}')
-        notes2.append((note.pitch, note.start, note.end-note.start))
 
-
+# Timestep - how small the new notes can be
 timestep = 0.125
 
 
 
-# create your MIDI object
-mf = MIDIFile(2)     # only 1 track
-track = 0   # the only track
+# Sets up the track
+mf = MIDIFile(2)
+track = 0
 
-time = 0    # start at the beginning
+time = 0
 mf.addTrackName(track, time, "crazytrack")
 mf.addTempo(track, time, 120)
 
-# mf.addTrackName(1, time, "maintrack")
-# mf.addTempo(1, time, 120)
-
-# add some notes
 channel = 0
 volume = 100
 
+
+# Finds the length of the piece
 end = 0
 for i in notes:
     if i[2] > end:
@@ -58,39 +49,18 @@ for i in notes:
 
 length = int(end/timestep)+5
 
+
+# Generates a dictionary of nodes at each time interval
 notesdict = {}
 for i in range(length):
     notesdict[i] = []
 
 for i in notes:
-    #mf.addNote(track, channel, i[0], i[1], i[2], volume)
     for x in range(math.ceil(i[1]/timestep), math.floor(i[2]/timestep)):
         notesdict[x].append(i[0])
 
 
-# outnotelist = []
-# prevlist = []
-# newlist = []
-# chorddict = {}
-# for i in notesdict:
-#     chorddict[i] = iden(notesdict[i])
-#     newlist = []
-#     if chorddict[i] is not None:
-#         for y in range(0, 5):
-#             center = (random.choice(notesdict[i])//12)*12
-#             pitch = (random.choice(chorddict[i])+int(np.random.normal(0, 0.001))*12+center) % 120
-#             newlist.append(pitch)
-#             if pitch not in prevlist or random.randint(0, 100) > 80:
-#                 #mf.addNote(track, channel, pitch, i*timestep, timestep, volume)
-#                 newnote = [pitch, i*timestep, timestep]
-#                 outnotelist.append(newnote)
-#                 newlist.append(newnote)
-#             else:
-#                 ournote = None
-#                 for i in prevlist:
-#                     if i == pitch
-#     prevlist = newlist
-
+# Expands the song by adding new notes within the chords of the time intervals
 outnotelist = []
 prevlist = []
 newlist = []
@@ -99,12 +69,16 @@ for i in notesdict:
     chorddict[i] = iden(notesdict[i])
     newlist = []
     if chorddict[i] is not None:
+        # Creates new notes
         for y in range(0, 5):
+            # Generates pitch out of allowed notes in chord - makes notes closer to the notes in the original music
             example = random.choice(notesdict[i])
             center = (example//12)*12
             pitch = (random.choice(chorddict[i])+int(np.random.normal(0, 0.001))*12+center) % 120
             if pitch % 12 != example % 12:
                 pitch = (random.choice(chorddict[i])+int(np.random.normal(0, 0.001))*12+center) % 120
+
+            # Checks if note was there in the previous step
             inthing = False
             place = None
             for j in prevlist:
@@ -112,66 +86,31 @@ for i in notesdict:
                     inthing = True
                     place = j
                     break
+
+            # if it is not there, it is repeated, and it is also repeated by a small chance if it is
             if not inthing or random.randint(0, 100) > 80:
-                #mf.addNote(track, channel, pitch, i*timestep, timestep, volume)
                 newnote = [pitch, i*timestep, timestep]
                 outnotelist.append(newnote)
                 newlist.append(newnote)
-            else:
+            else:  # extend the previous notes
                 place[2] += timestep
                 newlist.append(place)
+
+        # If the previous notes still fit have a high chance of making them longer
         for k in prevlist:
             if k not in newlist and k[0] % 12 in chorddict[i] and random.randint(1, 4) > 1:
                 k[2] += timestep
-
-
+        
     prevlist = newlist
 
+
+# Write all the notes
 for i in outnotelist:
     mf.addNote(track, channel, i[0], i[1], i[2], volume)
 
 
 
-
-
-
-# for i in notes:
-#     mf.addNote(track, channel, i[0], i[1], i[2], volume)
-
-#     for y in range(0, 5):
-
-#             a = random.randrange(0, 16)
-#             if a < 8:
-#                 pitch = (int(np.random.normal(0, 0.001))*12 + i[0]) % 120
-#             elif a < 12:
-#                 pitch = (int(np.random.normal(0, 0.001))*12 + i[0]+3) % 120
-#             else:
-#                 pitch = (int(np.random.normal(0, 0.001))*12 + i[0]-5) % 120
-#             time1 = np.random.uniform(i[1], i[1]+i[2])
-#             time2 = np.random.uniform(i[1], i[1]+i[2])
-#             if 0 <= pitch and pitch <= 127:
-#                 mf.addNote(track, channel, pitch, min(time1, time2), abs(time1-time2), volume)
-
-# for i in notes2:
-#     mf.addNote(1, channel, i[0], i[1], i[2], 50)
-
-
-# pitch = 60           # C4 (middle C)
-# time = 0             # start on beat 0
-# duration = 1         # 1 beat long
-# mf.addNote(track, channel, pitch, time, duration, volume)
-
-# pitch = 64           # E4
-# time = 2             # start on beat 2
-# duration = 1         # 1 beat long
-# mf.addNote(track, channel, pitch, time, duration, volume)
-
-# pitch = 67           # G4
-# time = 4             # start on beat 4
-# duration = 1         # 1 beat long
-# mf.addNote(track, channel, pitch, time, duration, volume)
-
-# write it to disk
+# Write it to ourput file
 while True:
     try:
         with open("output_chorded.mid", 'wb') as outf:
